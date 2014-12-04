a4cd2ae955ef126054dc1118867f9c4fc5ea38c9
J. Armando Uch <jahrmando@gmail.com>
2014-12-03 18:02:12 -0600

    Se agrego la funcion killgrep para matar procesos apartir de una busqueda regex

---
+++ functions/killgrep.fish
@@ -0,0 +1,7 @@
+function killgrep --description "Kill processes from a lookup"
+    if set -q $argv
+        echo "You have to write an argument"
+    else
+        command ps fuax | grep $argv | awk '{print $2}' | xargs kill -9 > /dev/null 2>&1
+    end
+end
